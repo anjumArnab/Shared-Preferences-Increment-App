ea33c26559b6eba1381a33ffb2c125d1eda9ef11
anjumArnab <anjumarnab050@gmail.com>
2025-03-29 21:27:26 +0600

    provider and tilt functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: const TiltCounter(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TiltCounter extends StatelessWidget {
+  const TiltCounter({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shared Preferences',
+      title: 'Tilt Counter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: const MyHomePage(title: 'Shared Preferences Counter App'),
+      home: const MyHomePage(title: 'Tilt Counter'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class CounterProvider extends ChangeNotifier {
+  int _counter = 0;
+  double _angle = 0.0;
+
+  int get counter => _counter;
+  double get angle => _angle;
+
+  CounterProvider() {
+    _loadCounter();
+  }
+
+  void _loadCounter() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    _counter = pref.getInt("counter") ?? 0;
+    notifyListeners();
+  }
+
+  void _saveCounter() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setInt("counter", _counter);
+  }
+
+  void updateTilt(double delta) {
+    _angle += delta * 0.1;
+    if (_angle > 0.1) {
+      _counter++;
+      _angle = 0.0;
+    } else if (_angle < -0.1) {
+      _counter--;
+      _angle = 0.0;
+    }
+    _saveCounter();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  _increment() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    setState(() {
-      _counter++;
-      pref.setInt("counter", _counter);
-    });
-  }
-
-  _loadCounter() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    setState(() {
-      _counter = pref.getInt("counter") ?? 0;
-    });
-  }
-
-  @override
-  void initState() {
-    _loadCounter();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final counterProvider = Provider.of<CounterProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         centerTitle: true,
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'You have tilted the box this many times:',
+          ),
+          Text(
+            '${counterProvider.counter}',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(height: 40),
+          Center(
+            child: GestureDetector(
+              onHorizontalDragUpdate: (details) =>
+                  counterProvider.updateTilt(details.delta.dx),
+              child: Transform.rotate(
+                angle: counterProvider.angle,
+                child: Container(
+                  width: 100,
+                  height: 30,
+                  color: Colors.lime,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Tilt Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: ClipOval(
-        child: FloatingActionButton(
-          onPressed: _increment,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
+          ),
+        ],
       ),
     );
   }
